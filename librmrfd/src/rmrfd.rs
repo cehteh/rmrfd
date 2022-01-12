a9b29d471f480040898ff4bc183eba267040366a
Christian Thäter <ct@pipapo.org>
2022-01-12 17:38:31 +0100

    rewrite the inventory, usnig threads instead shards

--- librmrfd/src/rmrfd.rs
+++ librmrfd/src/rmrfd.rs
@@ -5,10 +5,14 @@ use std::ffi::OsStr;
 use std::collections::HashMap;
 use std::os::unix::fs::MetadataExt;
 
+#[allow(unused_imports)]
+use log::{debug, error, info, trace, warn};
 use dirinventory::{
     openat, openat::metadata_types, Dir, DynResult, Gatherer, GathererBuilder, GathererHandle,
-    ObjectPath,
+    InternedName, ObjectPath, ProcessEntry,
 };
+
+use crate::inventory::{Inventory, ObjectKey};
 
 /// The daemon state
 pub struct Rmrfd {
@@ -29,6 +33,7 @@ pub struct RmrfdBuilder {
     gatherer_builder: GathererBuilder,
     min_blockcount:   metadata_types::blksize_t,
     rmrf_dirs:        HashMap<Arc<ObjectPath>, metadata_types::dev_t>,
+    rmrf_armed:       bool,
 }
 
 impl Default for RmrfdBuilder {
@@ -40,6 +45,7 @@ impl Default for RmrfdBuilder {
             /// give no much benefit when deleting in size order.
             min_blockcount:   64,
             rmrf_dirs:        HashMap::new(),
+            rmrf_armed:       false,
         }
     }
 }
@@ -49,24 +55,42 @@ impl RmrfdBuilder {
     /// the InventoryGatherer should in most cases be much faster than the directory worker
     /// threads. Thus this number can be small.
     pub fn with_inventory_backlog(mut self, n: usize) -> Self {
+        self.rmrf_armed = false;
         self.gatherer_builder = self.gatherer_builder.with_inventory_backlog(n);
         self
     }
 
-    /// How many worker threads are used to gather the inventory
-    pub fn with_inventory_threads(mut self, n: usize) -> Self {
+    /// How many worker threads are used to gather the inventory.
+    pub fn with_gather_threads(mut self, n: usize) -> Self {
+        self.rmrf_armed = false;
         self.gatherer_builder = self.gatherer_builder.with_gather_threads(n);
+        self
+    }
+
+    /// The number of threads the inventory uses to process entries.
+    pub fn with_inventory_threads(mut self, n: usize) -> Self {
+        self.rmrf_armed = false;
+        self.gatherer_builder = self.gatherer_builder.with_output_channels(n);
         self
     }
 
     /// Filter for files only larger than these much (512 byte) blocks.
     pub fn with_min_blockcount(mut self, c: metadata_types::blksize_t) -> Self {
+        self.rmrf_armed = false;
         self.min_blockcount = c;
+        self
+    }
+
+    /// Safety switch, without arming nothing will be deleted, used for testing and do nothing
+    /// options. Arming must be the last call before '.start()'.
+    pub fn arm(mut self, state: bool) -> Self {
+        self.rmrf_armed = state;
         self
     }
 
     /// register rmrf directories that are watched for deleting entries.
     pub fn add_dir(mut self, dir: &OsStr) -> io::Result<Self> {
+        self.rmrf_armed = false;
         let canonical_path = fs::canonicalize(dir)?;
         if !canonical_path.is_dir() {
             return Err(io::Error::from(io::ErrorKind::NotADirectory));
@@ -76,38 +100,75 @@ impl RmrfdBuilder {
         Ok(self)
     }
 
-    /// Creates and starts the Rmrfd.
-    pub fn run(self) -> io::Result<Rmrfd> {
-        let (inventory_gatherer, receiver) = self.gatherer_builder.start(Box::new(
-            move |gatherer: GathererHandle,
-                  entry: openat::Entry,
-                  parent_path: Arc<ObjectPath>,
-                  parent_dir: Arc<Dir>|
-                  -> DynResult<()> {
-                match entry.simple_type() {
-                    // recurse subdirs
-                    Some(openat::SimpleType::Dir) => {
-                        gatherer.traverse_dir(&entry, parent_path.clone(), parent_dir.clone());
-                        Ok(())
-                    }
-                    // everything else is eligible for deletion
-                    _ => match parent_dir.metadata(entry.file_name()) {
-                        Ok(metadata) => {
-                            if metadata.size().unwrap_or(0) > self.min_blockcount {
-                                gatherer.output_metadata(&entry, parent_path, metadata);
-                            }
-                            Ok(())
+    /// Creates the Rmrfd and starts worker threads.
+    pub fn start(self) -> io::Result<Rmrfd> {
+        info!("armed: {}", self.rmrf_armed);
+        let inventory_gatherer = self.gatherer_builder.start(Box::new(
+            move |gatherer: GathererHandle, entry: ProcessEntry, parent_dir: Option<Arc<Dir>>| {
+                match entry {
+                    ProcessEntry::Result(Ok(entry), parent_path) => match entry.simple_type() {
+                        Some(openat::SimpleType::Dir) => {
+                            trace!(
+                                "gather: subdir: {:?}",
+                                parent_path
+                                    .clone()
+                                    .subobject(InternedName::new(entry.file_name()))
+                            );
+                            gatherer.traverse_dir(&entry, parent_path, parent_dir);
                         }
-                        Err(e) => Err(Box::new(e)),
+                        _ => match parent_dir.unwrap().metadata(entry.file_name()) {
+                            Ok(metadata) => {
+                                trace!(
+                                    "gather: metadata: {:?}",
+                                    parent_path
+                                        .clone()
+                                        .subobject(InternedName::new(entry.file_name()))
+                                );
+                                if metadata.size().unwrap_or(0) > self.min_blockcount {
+                                    gatherer.output_metadata(
+                                        ObjectKey::try_from(&metadata)
+                                            .map_or(0, |key| key.bucket_hash()),
+                                        &entry,
+                                        parent_path,
+                                        metadata,
+                                    );
+                                }
+                            }
+                            Err(err) => {
+                                // FIXME: channel
+                                gatherer.output_error(0, Box::new(err), parent_path);
+                            }
+                        },
                     },
+                    ProcessEntry::Result(Err(err), parent_path) => {
+                        // FIXME: channel
+                        gatherer.output_error(0, Box::new(err), parent_path);
+                    }
+                    ProcessEntry::EndOfDirectory(_) => {}
                 }
             },
         ))?;
+
+        let inventory = Inventory::new(inventory_gatherer.channels_as_vec());
+
+        // create fastrmrf instance
+        // slowrmrf
 
         Ok(Rmrfd {
             inventory_gatherer,
             rmrf_dirs: self.rmrf_dirs,
         })
+    }
+
+    // directory watcher loop
+
+    #[cfg(test)]
+    pub fn delete_dir(&self, dir: ObjectPath) {
+        // rmrfd
+        //     .inventory_gatherer
+        //     .load_dir_recursive(ObjectPath::new("/home/ct/rmrfd_test"));
+
+        // wait for done
     }
 }
 
@@ -116,14 +177,36 @@ mod tests {
     use std::ffi::{OsStr, OsString};
 
     use crate::Rmrfd;
+    use crate::rmrfd::ObjectPath;
 
     #[test]
     fn smoke() {
         crate::tests::init_env_logging();
         let rmrfd = Rmrfd::build()
-            .add_dir(OsStr::new("../"))
-            .unwrap()
             .with_min_blockcount(64)
-            .run();
+            .with_inventory_threads(1)
+            .start();
+    }
+
+    #[test]
+    #[ignore]
+    fn rmtest() {
+        crate::tests::init_env_logging();
+        let rmrfd = Rmrfd::build()
+            .with_min_blockcount(1024)
+            .with_inventory_threads(8)
+            .start()
+            .unwrap();
+
+        // blocking things:
+
+        // rmrfd.watch_dirs
+        // rmrfd.delete_dir()
+
+        rmrfd
+            .inventory_gatherer
+            .load_dir_recursive(ObjectPath::new("./"));
+
+        std::thread::sleep(std::time::Duration::from_millis(1000));
     }
 }
